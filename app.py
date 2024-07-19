841cf7ed47ca3b67ae3539b25e3266f878d57dd7
Jemajr <164306732+Jemajr@users.noreply.github.com>
2024-07-19 05:45:43 +0000

    Added Dynamic Pages for each Job

--- app.py
+++ app.py
@@ -1,43 +1,8 @@
 from flask import Flask, render_template, jsonify
-from main import get_jobs
+from main import get_jobs, fetch_job
+
 
 app = Flask(__name__, static_folder='static')
-
-'''
-JOBS = [
-  {
-  'id': 1,
-  'title': 'Data Engineer',
-  'location': 'Seattle, WA',
-  'salary': '$150,000'
-  },
-  {
-  'id': 2,
-  'title': 'Software Developer',
-  'location': 'San Francisco, CA',
-  'salary': '$180,000'
-  },
-  {
-    'id': 3,
-  'title': 'Marketing Manager',
-  'location': 'New York, NY',
-  'salary': '$120,000'
-  },
-  {
-    'id': 4,
-  'title': 'UX/UI Designer',
-  'location': 'Austin, TX',
-  'salary': '$130,000'
-  },
-  {
-  'id': 5,
-  'title': 'Financial Analyst',
-  'location': 'Chicago, IL',
-  'salary': '$110,000'
-  }
-]
-'''
-
 
 
 @app.route("/")
@@ -46,10 +11,22 @@ def hello_world():
   return render_template('home.html',
                         jobs = JOBS)
 
+
 @app.route("/api/jobs")
 def list_jobs():
   JOBS = get_jobs()
   return jsonify(JOBS)
+
+
+@app.route("/job/<id>")
+def show_job(id):
+  JOB = fetch_job(id)
+  if not JOB:
+    return "Not Found", 404
+  return render_template('jobpage.html',
+                        job = JOB)
+  #return jsonify(job)
+
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0', debug=True)
--- main.py
+++ main.py
@@ -21,9 +21,38 @@ def get_jobs():
     cursor = connection.cursor()
     cursor.execute("SELECT * FROM jobs")
     result = cursor.fetchall()
-    #print(result)
+    
   finally:
     connection.close()
+    
   return result
 
-#print(get_jobs())
+def fetch_job(id):
+  timeout = 10
+  connection = pymysql.connect(
+    charset="utf8mb4",
+    connect_timeout=timeout,
+    cursorclass=pymysql.cursors.DictCursor,
+    db="defaultdb",
+    host=os.environ['DB_HOST'],
+    password=os.environ['DB_PASSWORD'],
+    read_timeout=timeout,
+    port=int(os.environ['DB_PORT']),
+    user="avnadmin",
+    write_timeout=timeout,
+  )
+
+  try:
+    cursor = connection.cursor()
+    cursor.execute("SELECT * FROM jobs WHERE id = %s", (id))
+    result = cursor.fetchall()
+    
+  finally:
+    connection.close()
+
+  print(result[0])
+  if len(result) == 0:
+    return None
+  else: 
+    return result[0]
+fetch_job(4)
